1f2bc0d1e7a2d923363ea69b7038e3d17236372a
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-17 11:14:31 +0530

    Added: Welcome Message.

---
+++ src/com/LineComparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.LineComparison;
+
+public class LineComparison {
+    public static void main(String args[]) {
+        System.out.println("Welcome to the Bridgelabz, let's begin with Line comparison computation.");
+    }
+}
